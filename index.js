660cbaabac4b61c1544bfd2d60b04f6f1b6a1806
MarkUA <mark.d.nahorniak@gmail.com>
2024-11-03 10:05:18 +0200

    fix

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-let i = 1
-while (i <= 10){
-    console.log(i)
-    i++
-}
+// let i = 1
+// while (i <= 10){
+//     console.log(i)
+//     i++
+// }
 
 for(let b = 2; b < 21; b += 1){
-if (b % 1 !== 0){
+if (b % 2 !== 0){
 continue;
 }    
 console.log(b)
 }
 
-for(let c = 7; c < 64; c += 7){
-console.log(c)
-}
+// for (let c = 7; c <= 64; c += 7){
+// console.log(`Таблиця множення на сім ${c}`)
+// }
 
-let d = [1, 2, 3, 4, 5]
+// let d = [1, 2, 3, 4, 5]
 
-while (i < d.length) {
-    console.log(d[i])
-    i++
-}
+// while (i < d.length) {
+//     console.log(d[i])
+//     i++
+// }
 
-let f = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let f = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for (let i = 1; i < f.length; i++) {
-    if (i === 7) {
-        break;
-    }
-    console.log(f[i])
-}
+// for (let i = 1; i < f.length; i++) {
+//     if (i === 7) {
+//         break;
+//     }
+//     console.log(f[i])
+// }
 
-let n = 20;
-for (let i = 0; i < 30; i++) {
-    if (i === n) {
-        break;
-    }
-    console.log(i)
-}
+// let n = 20;
+// for (let i = 0; i < 30; i++) {
+//     if (i === n) {
+//         break;
+//     }
+//     console.log(i)
+// }
 
-let g = 1;
-while (g <= 20) {
-    if (g % 3 === 0) {
-        g++
-        continue;
-    }
-    console.log(g)
-    g++
-}
+// let g = 1;
+// while (g <= 20) {
+//     if (g % 3 === 0) {
+//         g++
+//         continue;
+//     }
+//     console.log(g)
+//     g++
+// }
